85e4a42c8fe9d0dc32f9206809e11e3fd3d474c8
roryk <rory.kirchner@gmail.com>
2013-07-26 16:19:54 +0200

    Added combining the counts from htseq-count into one file.

--- src/bcbio.rnaseq/rnaseq.clj
+++ src/bcbio.rnaseq/rnaseq.clj
@@ -1,8 +1,10 @@
 (ns bcbio.rnaseq
   (:use [clostache.parser :only [render]]
         [clojure.java.shell :only [sh]]
-        [incanter.io :only [read-dataset]]))
-
+        [incanter.io :only [read-dataset]]
+        [incanter.core])
+  (:require [clj-yaml.core :as yaml]
+            [me.raynes.fs :as fs]))
 
 (spit "/Users/rory/cache/bcbio.rnaseq/resources/edgeR.R"
  (render (slurp "/Users/rory/cache/bcbio.rnaseq/resources/edgeR.template")
@@ -12,4 +14,44 @@
    :group "group"
    :out_file "out_file"}))
 
-(sh "Rscript" "/Users/rory/cache/bcbio.rnaseq/resources/edgeR.R")
+(sh "Rscript" "/Users/rory/cache/bcbio.rnaseq/resources/edgeR.template")
+
+(def config-file "/Users/rory/cache/bcbio.rnaseq/resources/bcbio_sample.yaml")
+(def config (yaml/parse-string (slurp config-file)))
+
+(defn get-config [config-file]
+  (yaml/parse-string (slurp config-file)))
+(defn- get-bcbio-samples [config]
+    (map :description (:details config)))
+
+(defn- construct-htseq-filenames [base-dir config]
+  (map #(str base-dir "/htseq-count/" % ".counts") (get-bcbio-samples config)))
+
+(defn load-htseq [htseq-file]
+  (read-dataset htseq-file :delim \tab))
+
+(defn htseq-files [config-file]
+  (let [config (get-config config-file)
+        base-dir (fs/parent config-file)]
+    (construct-htseq-filenames base-dir config)))
+
+(def count-files (htseq-files config-file))
+
+(defn extract-count-column [htseq-file]
+  (to-dataset (sel (load-htseq htseq-file) :cols 1)))
+
+(defn merge-htseq-counts [combined htseq-file]
+  (conj-cols combined (extract-count-column htseq-file)))
+
+(defn base-filename [filename]
+  (fs/split-ext (fs/base-name filename)))
+
+(defn base-stem [filename]
+  (first (base-filename filename)))
+
+(defn combine-count-files [config-file]
+;  (let (out-file (str (fs/parent config-file) "/htseq-count/combined.counts"))
+  (col-names
+   (reduce merge-htseq-counts (load-htseq (first count-files))
+           (rest count-files))
+   (cons "id" (map base-stem count-files))))
