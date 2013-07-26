d654ee6ecfd0df6be9e8ee96745b266c311dbbab
roryk <rory.kirchner@gmail.com>
2013-07-26 16:19:34 +0200

    Added a few dependencies and bumped JVM opts up.

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject bcbio.rnaseq "0.0.1-SNAPSHOT"
-  :description "Cool new project to do things and stuff"
+  :description "Run mutiple DE callers on RNA-seq data"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [de.ubercode.clostache/clostache "1.3.1"]
                  [incanter/incanter-io "1.5.1"]
-                 [me.raynes/fs "1.4.4"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+                 [me.raynes/fs "1.4.4"]
+                 [clj-yaml "0.4.0"]
+                 [org.clojure/data.json "0.2.2"]
+                 [clj-http "0.7.5"]]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
+  :jvm-opts ["-Xmx2g"])
