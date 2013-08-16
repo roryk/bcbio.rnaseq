56ac371708f6da5d0f2707d3ad62dac890de68ca
roryk <rory.kirchner@gmail.com>
2013-08-16 03:03:24 -0400

    Added basic YAML file parsing.

--- src/bcbio.rnaseq/rnaseq.clj
+++ src/bcbio.rnaseq/rnaseq.clj
@@ -21,11 +21,18 @@
 
 (defn get-config [config-file]
   (yaml/parse-string (slurp config-file)))
-(defn- get-bcbio-samples [config]
-    (map :description (:details config)))
+
+(defn- get-descriptions [config]
+  (map :description (:details config)))
+
+(defn get-metadata [config]
+  (map :metadata (:details config)))
+
+(defn get-condition [config]
+  (map :condition (get-metadata config)))
 
 (defn- construct-htseq-filenames [base-dir config]
-  (map #(str base-dir "/htseq-count/" % ".counts") (get-bcbio-samples config)))
+  (map #(str base-dir "/htseq-count/" % ".counts") (get-descriptions config)))
 
 (defn load-htseq [htseq-file]
   (read-dataset htseq-file :delim \tab))
@@ -49,9 +56,40 @@
 (defn base-stem [filename]
   (first (base-filename filename)))
 
-(defn combine-count-files [config-file]
-;  (let (out-file (str (fs/parent config-file) "/htseq-count/combined.counts"))
+(defn combine-count-files [count-files]
   (col-names
    (reduce merge-htseq-counts (load-htseq (first count-files))
            (rest count-files))
    (cons "id" (map base-stem count-files))))
+
+(declare that-function-to-write)
+
+(defn write-combined-count-file [count-files]
+  (let [out-file (str (fs/parent (first count-files)) "/combined.counts")]
+    (that-function-to-write (combine-count-files count-files) out-file)
+    (out-file)))
+
+(defn seq-to-R-list [xs]
+  " this needs to take a vec and turn it into c(v[0], v[1], v[2], etc)"
+  (str "c(" (clojure.string/join "," xs) ")"))
+
+
+(defn dirname [path]
+  (str (parent (expand-home test-path)) "/"))
+
+(defn change-extension [path extension]
+  (str (dirname path) (name path) extension))
+
+(defn temporary-R-file [path]
+  (temp-file (name path) ".R"))
+
+(defn write-template
+  "writes an R file out from a template and returns the template name"
+  [template count_file class]
+  (do
+    (spit (change-extension template ".R")
+          (render (slurp template) {:count_file count_file :class class}))
+    (change-extension template ".R")))
+
+(defn run-template [template count_file class]
+  (sh "Rscript" (write-template template count_file class)))
