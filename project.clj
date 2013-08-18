6933cb7ce0ba2d029e4020ab337fe62dd3790138
roryk <rory.kirchner@gmail.com>
2013-08-18 17:14:28 -0400

    Added combinatorics as a dependency.

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
                  [me.raynes/fs "1.4.4"]
                  [clj-yaml "0.4.0"]
                  [org.clojure/data.json "0.2.2"]
-                 [clj-http "0.7.5"]]
+                 [clj-http "0.7.5"]
+                 [org.clojure/math.combinatorics "0.0.4"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
   :jvm-opts ["-Xmx2g"])
