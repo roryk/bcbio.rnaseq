7c8dffac55c30c65b756fa926860c2a177e81bde
roryk <rory.kirchner@gmail.com>
2013-12-01 23:34:41 -0500

    SEQC raw data for testing against.

---
+++ resources/seqc/README.txt
@@ -0,0 +1,3 @@
+UHRR: group A, ERCC mix 1, total RNA from ten human cell lines
+HBRR: group B, ERCC mix 2, human brain reference from ambion.
+
---
+++ resources/seqc/qPCR/raw/analysis.R
@@ -0,0 +1,41 @@
+# tidy the taqman data
+library(reshape)
+library(stringr)
+
+fix_excel_symbols = function(symbols) {
+    return(unlist(lapply(as.character(symbols), fix_string)))
+}
+
+fix_string = function(x) {
+    regex_str = "([0-9])-(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)"
+    return(if(any(grep(regex_str, x))) date_fixer(x) else x)
+}
+
+date_fixer = function(x) {
+    tokens = str_split(x, "-")
+    return(paste(toupper(tokens[[1]][2]), tokens[[1]][1], sep=""))
+}
+
+description = data.frame(group=c("A", "B"), reference=c("universal", "brain"),
+    mix=c(1, 2))
+x = read.table("TAQ.txt", sep="\t", header=TRUE)
+x$id = fix_excel_symbols(x$X)
+x$X = NULL
+melted = melt(x)
+melted$group = apply(melted, 1, function (q) substring(q["variable"], 1, 1))
+melted$replicate = apply(melted, 1, function (q) substring(q["variable"], 2, 2))
+melted$variable = NULL
+melted = merge(melted, description, by=c("group"), all=TRUE)
+melted$method = "qPCR"
+
+casted = cast(melted, id +  reference + mix + group ~ ., mean)
+colnames(casted) = c("id", "reference", "mix", "group", "expression")
+
+foldchange = cast(casted, id ~ group)
+foldchange$logFC = log2(foldchange$B/foldchange$A)
+foldchange = foldchange[, c("id", "logFC")]
+foldchange$method = "qPCR"
+
+write.table(foldchange, "foldchange_qpcr.tidy", row.names=FALSE, quote=FALSE, col.names=TRUE,
+            sep="\t")
+write.table(casted, "qpcr.tidy", row.names=FALSE, quote=FALSE, col.names=TRUE, sep="\t")
