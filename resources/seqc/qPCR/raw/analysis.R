34fb9e3f93f8057ade3590f1ae1aee2a6a25750c
roryk <rory.kirchner@gmail.com>
2013-12-24 03:12:38 -0500

    Added ROC plot and AUC plot.

--- resources/seqc/qPCR/raw/analysis.R
+++ resources/seqc/qPCR/raw/analysis.R
@@ -1,6 +1,7 @@
 # tidy the taqman data
 library(reshape)
 library(stringr)
+library(methods)
 
 fix_excel_symbols = function(symbols) {
     return(unlist(lapply(as.character(symbols), fix_string)))
@@ -16,11 +17,18 @@ date_fixer = function(x) {
     return(paste(toupper(tokens[[1]][2]), tokens[[1]][1], sep=""))
 }
 
+t.test.robust <- function(...) {
+    obj<-try(t.test(...), silent=TRUE)
+    if (is(obj, "try-error")) return(NA) else return(obj$p.value)
+}
+
 description = data.frame(group=c("A", "B"), reference=c("universal", "brain"),
     mix=c(1, 2))
 x = read.table("TAQ.txt", sep="\t", header=TRUE)
 x$id = fix_excel_symbols(x$X)
+x = x[!duplicated(x$id), ]
 x$X = NULL
+
 melted = melt(x)
 melted$group = apply(melted, 1, function (q) substring(q["variable"], 1, 1))
 melted$replicate = apply(melted, 1, function (q) substring(q["variable"], 2, 2))
@@ -33,7 +41,9 @@ colnames(casted) = c("id", "reference", "mix", "group", "expression")
 
 foldchange = cast(casted, id ~ group)
 foldchange$logFC = log2(foldchange$B/foldchange$A)
-foldchange = foldchange[, c("id", "logFC")]
+foldchange$pvalue =  unlist(dlply(x, "id", function(y) t.test.robust(y[,c("A1", "A2", "A3", "A4")], y[,c("B1", "B2", "B3", "B4")])))
+foldchange$padj = p.adjust(foldchange$pvalue)
+foldchange = foldchange[, c("id", "logFC", "pvalue", "padj")]
 foldchange$method = "qPCR"
 
 write.table(foldchange, "foldchange_qpcr.tidy", row.names=FALSE, quote=FALSE, col.names=TRUE,
