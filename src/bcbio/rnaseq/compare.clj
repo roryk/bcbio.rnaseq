6099cd100f955f996040f6fe7e31144bd5ba3adc
roryk <rory.kirchner@gmail.com>
2013-12-30 17:48:43 -0500

    Fixed capitalization error.

--- src/bcbio/rnaseq/compare.clj
+++ src/bcbio/rnaseq/compare.clj
@@ -14,7 +14,7 @@
   "create a fold change plot comparing to the seqc qPCR data"
   (let [template-file (get-resource "comparisons/qPCR_foldchange.template")
         out-file (swap-directory "fc-plot.pdf" (analysis-dir))
-        qpcr-file (get-resource "seqc/qpcr/qpcr_HBRR_vs_UHRR.tidy")
+        qpcr-file (get-resource "seqc/qPCR/qpcr_HBRR_vs_UHRR.tidy")
         template-config {:out-file (escape-quote out-file)
                          :qpcr-file (escape-quote qpcr-file)
                          :in-files (seq-to-rlist in-files)}]
