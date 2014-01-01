64582b16f6b05d9c76795dcb38e376bf20fd79fb
roryk <rory.kirchner@gmail.com>
2014-01-01 16:33:58 -0500

    Added baySeq support.

--- resources/scripts/install_libraries.R
+++ resources/scripts/install_libraries.R
@@ -39,7 +39,7 @@ install.github = function(packages, username) {
 cran_packages = c("devtools", "ggplot2", "reshape", "pROC", "plyr")
 install.cran(cran_packages)
 
-bioconductor_packages = c("edgeR", "HTSFilter", "DESeq2")
+bioconductor_packages = c("edgeR", "HTSFilter", "DESeq2", "baySeq")
 source("http://bioconductor.org/biocLite.R")
 install.bioconductor(bioconductor_packages)
 
