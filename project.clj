628ceddb7c1446d4a6aab1ba4402e21669a681a6
roryk <rory.kirchner@gmail.com>
2014-01-30 14:24:00 -0500

    Update fs to 1.4.5 to handle java 6.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [de.ubercode.clostache/clostache "1.3.1"]
                  [incanter/incanter-io "1.5.1"]
-                 [me.raynes/fs "1.4.4"]
+                 [me.raynes/fs "1.4.5"]
                  [clj-yaml "0.4.0"]
                  [org.clojure/data.json "0.2.2"]
                  [clj-http "0.7.5"]
