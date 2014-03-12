c626da0f97d7905cb0e4a4dc33f6e23e8cd12b27
roryk <rory.kirchner@gmail.com>
2014-03-12 18:53:05 -0400

    Fixed dependencies.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Run mutiple DE callers on RNA-seq data"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [de.ubercode.clostache/clostache "1.3.1"]
+                 [incanter "1.5.4"]
                  [incanter/incanter-io "1.5.1"]
                  [me.raynes/fs "1.4.5"]
                  [clj-yaml "0.4.0"]
@@ -11,8 +12,8 @@
                  [org.clojure/tools.cli "0.3.0"]
                  [bcbio.run "0.0.1-SNAPSHOT"]
                  [clj-http "0.7.8"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
-  :plugins [[lein-midje "3.0.1"]]
+  :profiles {:dev {:dependencies [[midje "1.6.2"]]}}
+  :plugins [[lein-midje "3.1.3"]]
   :resource-paths ["resources"]
   :jvm-opts ["-Xmx2g"]
   :aot [bcbio.rnaseq.core]
