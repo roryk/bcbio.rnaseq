202c87f24121caee1f1cccd06f5fa9408f41c5ca
roryk <rory.kirchner@gmail.com>
2015-12-16 12:38:58 -0500

    Add --version option.
    Snagged from @chapmanb in his bcbio.variation.recall repo.

--- src/bcbio/rnaseq/core.clj
+++ src/bcbio/rnaseq/core.clj
@@ -3,12 +3,24 @@
             [bcbio.rnaseq.compare :as compare]
             [bcbio.rnaseq.simulate :as simulate]
             [clojure.string :as string]
+            [clojure.java.io :as io]
             [clojure.tools.cli :refer [parse-opts]])
   (:gen-class :main true))
 
+(defn version
+  "lookup version, snagged from bcbio.variation.recall"
+  []
+  (with-open [reader
+              (-> "META-INF/maven/bcbio.rnaseq/bcbio.rnaseq/pom.properties"
+                  io/resource
+                  io/reader)]
+    (-> (doto (java.util.Properties.)
+          (.load reader))
+        (.getProperty "version"))))
 
-(def options
+(def cli-options
   [["-h" "--help"]
+   ["-v" "--version"]
    ["-n" "--cores CORES" "Number of cores"
     :default 1
     :parse-fn #(Integer/parseInt %)
@@ -45,7 +57,8 @@
     :entry simulate/simulate-cli}
    {:name "summarize"
     :description "summarize the QC data from a bcbio-nextgen run"
-    :entry qcsummary/summarize-cli}])
+    :entry qcsummary/summarize-cli}
+   ])
 
 (defn subcommand-to-string [{:keys [name description]}]
   (str "  " name ": " description \newline))
@@ -70,9 +83,11 @@
   (:entry (first (filter #(= (:name %) cmd) subcommands))))
 
 (defn -main [& args]
-  (let [{:keys [options arguments errors summary]} (parse-opts args options :in-order true)
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options
+                                                               :in-order true)
         cmd (first arguments)]
     (cond
-     (:help options) (exit 0 (subcommand-help subcommands))
-     (not (has-subcommand? cmd)) (exit 0 (subcommand-help subcommands)))
+      (:version options) (exit 0 (str "bcbio.rnaseq: " (version)))
+      (:help options) (exit 0 (subcommand-help subcommands))
+      (not (has-subcommand? cmd)) (exit 0 (subcommand-help subcommands)))
     (apply (cli-entry cmd) (rest arguments))))
