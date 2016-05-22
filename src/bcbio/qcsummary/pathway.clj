2b7dd3138d44602ef253d3e81baf5e61e7930856
roryk <rory.kirchner@gmail.com>
2016-05-22 16:01:19 -0400

    Use the string instead of the object for the database.
    Switch to using biomaRt.

--- src/bcbio/qcsummary/pathway.clj
+++ src/bcbio/qcsummary/pathway.clj
@@ -2,12 +2,17 @@
   (:require [bcbio.rnaseq.util :as util]
             [bcbio.qcsummary.r :refer [write-template]]))
 
-(defrecord OrgInfo [name db symbol])
-(def org-dbs {:mouse (OrgInfo. "mouse" "org.Mm.eg.db" "mgi_symbol")
-              :human (OrgInfo. "human" "org.Hs.eg.db" "hgnc_symbol")})
+(defrecord OrgInfo [name db symbol biomart-dataset])
+(def org-dbs {:mouse (OrgInfo. "mouse" "org.Mm.eg.db" "mgi_symbol"
+                               "mmusculus_gene_ensembl")
+              :human (OrgInfo. "human" "org.Hs.eg.db" "hgnc_symbol"
+                               "hsapiens_gene_ensembl")})
 
 (def pathway-template "bcbio/pathway.template")
 
 (defn write-pathway-template [out-dir organism]
-  (let [pathway-config {:orgdb (util/escape-quote (-> org-dbs organism :db))}]
+  (let [pathway-config
+        {:orgdb (util/escape-quote (-> org-dbs organism :db))
+         :biomart-dataset (util/escape-quote
+                           (-> org-dbs organism :biomart-dataset))}]
     (write-template pathway-template pathway-config out-dir ".pathway")))
