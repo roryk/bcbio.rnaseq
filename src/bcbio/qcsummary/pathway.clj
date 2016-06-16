6d935901819c2da825119f2c16bc576b1a0c67ef
roryk <rory.kirchner@gmail.com>
2016-06-16 10:36:48 -0400

    Add supprt for KEGG GSEA analysis.

--- src/bcbio/qcsummary/pathway.clj
+++ src/bcbio/qcsummary/pathway.clj
@@ -2,11 +2,11 @@
   (:require [bcbio.rnaseq.util :as util]
             [bcbio.qcsummary.r :refer [write-template]]))
 
-(defrecord OrgInfo [name db symbol biomart-dataset])
+(defrecord OrgInfo [name db symbol biomart-dataset keggname])
 (def org-dbs {:mouse (OrgInfo. "mouse" "org.Mm.eg.db" "mgi_symbol"
-                               "mmusculus_gene_ensembl")
+                               "mmusculus_gene_ensembl" "mmu")
               :human (OrgInfo. "human" "org.Hs.eg.db" "hgnc_symbol"
-                               "hsapiens_gene_ensembl")})
+                               "hsapiens_gene_ensembl" "hsa")})
 
 (def pathway-template "bcbio/pathway.template")
 
@@ -14,5 +14,7 @@
   (let [pathway-config
         {:orgdb (util/escape-quote (-> org-dbs organism :db))
          :biomart-dataset (util/escape-quote
-                           (-> org-dbs organism :biomart-dataset))}]
+                           (-> org-dbs organism :biomart-dataset))
+         :keggname (util/escape-quote
+                    (-> org-dbs organism :keggname))}]
     (write-template pathway-template pathway-config out-dir ".pathway")))
