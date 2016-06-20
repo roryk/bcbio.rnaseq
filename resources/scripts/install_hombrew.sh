4e469e33ba943919d41412d192b4c2041f4606eb
roryk <rory.kirchner@gmail.com>
2016-06-20 10:54:31 -0400

    Remove deprecated homebrew script.

--- resources/scripts/install_hombrew.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -d "$HOME/.linuxbrew" ]; then
-    echo "homebrew found, skipping installation."
-else
-    echo -ne '\n' | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
-fi
